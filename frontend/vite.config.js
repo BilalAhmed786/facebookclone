79d824f37487f1a55dd080d45ac494b4e0b254bb
BilalAhmed786 <69997357+BilalAhmed786@users.noreply.github.com>
2024-11-08 12:04:59 +0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,  // Allows external network connections
-    port: 80,    // Sets the server port to 80
+    port: 5173,
     proxy: {
       '/api': {
         target: 'http://localhost:4000',
@@ -15,5 +14,6 @@ export default defineConfig({
     },
   },
 });
+
 
 
