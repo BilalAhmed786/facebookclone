ab21421b392631dc7df19afdc36c3fcf39cb5d8e
BilalAhmed786 <fasst.sallar@gmail.com>
2026-09-21 14:49:28 +0500

    deploy cloudflare

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}))   
 app.use(cors({
 
-    origin:'http://localhost:5173',
+    origin:'https://facebookclone.fasst-sallar.workers.dev',
     credentials:true
 
 }))
